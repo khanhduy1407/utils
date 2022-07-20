007aec9a356daf237c8cb63cd685001d28dfa8ed
NKDuy <kn145660@gmail.com>
2022-07-20 08:27:35 +0700

    chore: release candidate 0 for v0.5.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# @nkduy/utils
+
+Opinionated collection of common JavaScript / TypeScript utils by [@nkduy](https://github.com/khanhduy1407).
+
+- Tree-shakable ESM
+- Fully typed - with TSDocs
+- Type utilities - `Arrayable<T>`, `ElementOf<T>`, etc.
+
+> This package is designed to be used as `devDependencies` and bundled into your dist.
+
+## License
+
+[MIT](./LICENSE) License
---
+++ src/promise.ts
@@ -0,0 +1,90 @@
+import { remove } from './array'
+import type { Fn } from './types'
+
+export interface SingletonPromiseReturn<T> {
+  (): Promise<T>
+  /**
+   * Reset current staled promise.
+   * Await it to have proper shutdown.
+   */
+  reset: () => Promise<void>
+}
+
+/**
+ * Create singleton promise function
+ *
+ * @category Promise
+ */
+export function createSingletonPromise<T>(fn: () => Promise<T>): SingletonPromiseReturn<T> {
+  let _promise: Promise<T> | undefined
+
+  function wrapper() {
+    if (!_promise)
+      _promise = fn()
+    return _promise
+  }
+  wrapper.reset = async() => {
+    const _prev = _promise
+    _promise = undefined
+    if (_prev)
+      await _prev
+  }
+
+  return wrapper
+}
+
+/**
+ * Promised `setTimeout`
+ *
+ * @category Promise
+ */
+export function sleep(ms: number, callback?: Fn<any>) {
+  return new Promise<void>(resolve =>
+    setTimeout(async() => {
+      await callback?.()
+      resolve()
+    }, ms),
+  )
+}
+
+/**
+ * Create a promise lock
+ *
+ * @category Promise
+ * @example
+ * ```
+ * const lock = createPromiseLock()
+ *
+ * lock.run(async () => {
+ *   await doSomething()
+ * })
+ *
+ * // in anther context:
+ * await lock.wait() // it will wait all tasking finished
+ * ```
+ */
+export function createPromiseLock() {
+  const locks: Promise<any>[] = []
+
+  return {
+    async run<T = void>(fn: () => Promise<T>): Promise<T> {
+      const p = fn()
+      locks.push(p)
+      try {
+        return await p
+      }
+      finally {
+        remove(locks, p)
+      }
+    },
+    async wait(): Promise<void> {
+      await Promise.allSettled(locks)
+    },
+    isWaiting() {
+      return Boolean(locks.length)
+    },
+    clear() {
+      locks.length = 0
+    },
+  }
+}
